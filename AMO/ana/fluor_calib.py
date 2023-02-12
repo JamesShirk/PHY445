aea15e04b7411a8c2a97ee6b3fb30fa49f806f62
James Shirk <jamesshirk@Jamess-MBP.home>
2023-02-12 14:39:02 -0500

    reformatted fitting code, finished primary calibration plot

--- AMO/ana/fluor_calib.py
+++ AMO/ana/fluor_calib.py
@@ -1,11 +1,10 @@
 import numpy as np
-from scipy.signal import find_peaks
 from scipy.optimize import curve_fit
-from scipy.fft import fft, ifft, fftfreq
 import sys
 import pandas as pd
 import matplotlib.pyplot as plt
-from itertools import chain
+
+import fitPeak as fp
 
 # def fitWrapper(x, N, *args):
 #     bck = args[0]
@@ -14,81 +13,80 @@ from itertools import chain
 #         gaus.append(args[3*i, 3*i+2])
 #     return fit(x, bck, gaus)
 
-def initParam(elem, peaks):
-    amps = elem.iloc[peaks].tolist()
-    means = elem.index[peaks]
-    print(means)
-    sigmas = np.ones_like(amps)*5
-    bck = [10, 0, 0]
-    return np.array(list(chain(bck, amps, means, sigmas)), dtype = np.float64)
 
-def fit(x, *args):
-    if type(args[0]) is np.ndarray:
-        args = np.array(args[0], dtype = np.float64)
-    f = np.zeros_like(x, dtype = np.float64)
-    gauss = np.array(args[3:], dtype = np.float64).reshape(3, len(args[1:])//3).T
-    for i in gauss:
-        f += i[0]*np.exp((-(x - i[1])**2) / (2*i[2]))
-    return f + args[0] + args[1]*x + args[2]*x**2
 
 def initialize_data(fname):
     df = pd.read_csv(fname, skiprows=[1, -1])
     # removes rows with all zeros, but preserves row numbers
     no_zero = df.loc[(df>20).any(axis=1)]
-    fig, ax = plt.subplots()
-    #ax.plot(no_zero)#, x = "channel", y = "counts")
-    ax = no_zero.plot(figsize= (20, 6))
-    #fig.set_size_inches(10, 6, forward = True)
-    ax.set_xlim([50, 900])
-    ax.set_ylim([10, 1e4])
-    ax.set_yscale("symlog")
-    ax.legend()
-    plt.savefig("test.png", dpi =100)
+    return no_zero
+
+def calibration(fname):
+    dat = initialize_data(fname)
+
+    calib = ['\'Fe\'', '\'Zn\'', '\'Cu\'']
+    color = ["red", "green", "blue"]
+    colorFit = ["purple", "orange", "m"]
+    fitParams = []
+    fitError = []
+
+    for i in calib:
+        elem = dat[i]
+        x = np.array(elem.index.tolist())
+        popt, pcov = curve_fit(fp.fit, xdata=x, ydata=elem, p0 = fp.initParam(elem))
+        fitParams.append(popt)
+        fitError.append(pcov)
+
+    fig, ax = plt.subplots(3, 1)
+    for i in range(len(calib)):
+        ax[i].plot(dat[calib[i]].index.tolist(),dat[calib[i]], color = color[i])
+        ax[i].plot(x, fp.fit(x, fitParams[i]), "--", color = colorFit[i])
+        ax[i].set_ylim([0, 1e4])
+        ax[i].set_yscale("symlog")
+        ax[i].grid(visible=True)
+        ax[i].margins(x=0)
+        ax[i].set_xlim([200, 400])
+        ax[i].set_title(calib[i])
+    fig.supylabel("Counts")
+    fig.supxlabel("Channel")
+    plt.tight_layout()
+    plt.savefig("testCalib.png", dpi = 100)
     plt.close()
-
-    dx = 1
     
+    # in kev, order of Fe, Zn, Cu
+    truePeaks = np.array([[6.40, 7.06], [8.64, 9.57], [8.05, 8.90]])
+    measuredPeaks = []
+    errorPeaks = []
+    for i in range(len(fitParams)):
+        gauss = np.array(fitParams[i][3:], dtype = np.float64).reshape(3, len(fitParams[i][3:])//3)
+        #gaussE = np.array(fitError[i][3:], dtype = np.float64).reshape(3, len(fitError[i][3:])//3)
+        measuredPeaks.append(gauss[1])
+        errorPeaks.append(gauss[2])
+    measuredPeaks = np.concatenate((measuredPeaks[0], measuredPeaks[1], measuredPeaks[2]), axis = None)
+    errorPeaks = np.concatenate((errorPeaks[0], errorPeaks[1], errorPeaks[2]), axis = None)
+    truePeaks = np.concatenate((truePeaks[0], truePeaks[1], truePeaks[2]), axis = None)
+
+    p = np.argsort(measuredPeaks)
+
+    a,b,cov_00,cov_11,cov_01,chi2 = fp.wlinear_fit(truePeaks[p],measuredPeaks[p],1.0/errorPeaks[p]**2)
+    x = np.linspace(5, 11, 1000)
+    y = b*x + a
+
+    plt.errorbar(truePeaks, measuredPeaks, yerr = errorPeaks, marker = "o", ls = 'None', label = 'Measured Peaks')
+    plt.plot(x, y, "--", label = 'Error Weighted Fit a:{0}, b:{1}'.format(round(a, 2), round(b, 2)))
+    plt.legend()
+    plt.xlabel("True Peaks (keV)")
+    plt.ylabel("Channel Number")
+    plt.title("Calibration with Fe, Zn, and Cu")
+    plt.grid()
+    plt.margins(x=0)
+    plt.savefig("testCorr.png")
     
-    peak_subset = no_zero['\'Rh\'']#.loc[200:300]
-
-
-    #curve_fit(fit, no_zero.index, no_zero['\'Fe\''])
-    peaks, _ = find_peaks(peak_subset, height = 10, prominence = dx*50)
-    x = np.array(peak_subset.index.tolist())
-    
-    popt, pcov = curve_fit(fit, xdata=x, ydata=peak_subset, p0 = initParam(peak_subset, peaks))
-
-
-    fig, ax = plt.subplots()
-    ax = peak_subset.plot(figsize= (20, 6))
-    ax.plot(x, fit(x, popt), "--")
-    #ax.set_xlim([50, 300])
-    ax.set_ylim([0, 1e4])
-    ax.set_yscale("symlog")
-    plt.savefig("testRh.png", dpi = 100)
-    plt.close()
-
-    return
-
-    Ft = fft(no_zero['\'Fe\''])
-    N = no_zero['\'Fe\''].shape[0]
-    xf = fftfreq(N, 1)#[:N//2]
-    print(xf)
-    xf = np.where(np.abs(xf) > 0.2)
-    print(xf)
-    cut = len(xf)//2
-    Ift = ifft(Ft[xf])
-    #xf = np.argmax(np.abs(xf)>0.2)
-
-    fig, ax = plt.subplots()
-    ax.plot(xf[0], Ift)
-    #ax.plot(xf, 2.0/N * np.abs(Ft[0:N//2].imag), "--")
-    plt.savefig("testFourier.png")
-
 
 
 if __name__ == "__main__":
     #try:
-    initialize_data(sys.argv[1])
+    #initialize_data(sys.argv[1])
+    calibration(sys.argv[1])
     #except:
     #    print("Run as \'python3 fluor_calib.py datafile\'")
